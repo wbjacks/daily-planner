63a23b7f405c626a8a7c3dd9394bb7fa750b4979
Jackson <wjackson@ea.com>
2018-04-08 22:41:12 -0700

    Got future planner working as a node app because TEX is really hard

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # daily-planner
-LaTeX file to generate a daily planner
+Was gonna be LaTeX but HTML is easier to work with
+
+# Places I stole code from
+ - http://www.diyplanner.com/node/6270 (inspo)
+
+
---
+++ main.js
@@ -0,0 +1,70 @@
+var _htmlPdf = require('html-pdf'),
+    _fs = require('fs'),
+    _handlebars = require('handlebars'),
+    _http = require('http'),
+    _moment = require('moment');
+
+function init() {
+    _handlebars.registerPartial('future-planner', _fs.readFileSync('./future_planner.html', 'utf8'));
+
+    _handlebars.registerHelper('calendar', function(month) {
+        var timestamp = _moment({
+            year: this.year,
+            month: _moment().month(month).format("M") - 1
+        });
+        var out = "<table class=\"calendar\"><tr><td>S</td><td>M</td><td>Tu</td><td>W</td><td>Th</td><td>F</td><td>S</td></tr>";
+        var printedDate = 0;
+
+        for (var row = 0; row < 5; row++) {
+            out += "<tr>";
+            for (var col = 0; col < 7; col++) {
+                out += "<td>";
+                if (printedDate > 0) {
+                    out += ++printedDate;
+                }
+                else if (timestamp.day() === col) {
+                    out += ++printedDate;
+                }
+                out += "</td>";
+            }
+            out += "</tr>";
+        }
+        return out + "</table>";
+    });
+
+    _handlebars.registerHelper('each-NPairs', function(items, options) {
+        if (!options.hash.N) {
+            throw "Argument N required to each-NPairs helper.";
+        }
+
+        var N = parseInt(options.hash.N),
+            out = "";
+        if (!N) {
+            throw "Argument N must be an int";
+        }
+
+        for (var i = 0; i < items.length; i += N) {
+            var context = {};
+            for (var j = 0; j < N; j++) {
+                context["item"+j] = items[i+j];
+            }
+            out += options.fn(context);
+        }
+        return out;
+    });
+
+    return _handlebars.compile(_fs.readFileSync('./index.html', 'utf8'));
+}
+
+if (process.argv.length === 3 && process.argv[3] === 'print') {
+
+}
+else {
+    _http.createServer(function(req, resp) {
+        var template = init();
+
+        resp.writeHead(200, {'Content-Type': 'text/html'});
+        resp.write(template({year: "2018", months: _moment.months()}));
+        resp.end();
+    }).listen(8080);
+}
